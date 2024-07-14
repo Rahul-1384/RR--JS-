658b073d964c43ca5cd86e3f5de2ba79138246a3
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-14 17:47:23 +0530

    s

---
+++ class-3.js
@@ -0,0 +1,21 @@
+// let date=new Date();
+
+// let date2=new Date(2004,6,2,12);
+// console.log(date2);
+
+
+let arr=[1,2,3,10,4,10,5,6,7];
+// console.log(arr[1]);
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]==10){
+//         console.log(i);
+//     }else{
+//         continue;
+//     }
+// }
+// arr.push(100);
+// arr.unshift(100);
+// arr.splice(3,2,100);
+// console.log(arr);
+
+console.log(arr.indexOf(10))
