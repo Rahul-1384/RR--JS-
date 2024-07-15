3941a5509ec142889fc54f4da4a4d998def0adba
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-15 16:00:20 +0530

    js basics complete

--- class-4.js
+++ class-4.js
@@ -7,8 +7,9 @@
 // }
 
 
-// arguments
 
+
+// arguments
 // function myFun(a,b){
 //     let total=0;
 //     for(let value of arguments){
@@ -18,6 +19,8 @@
 // }
 // let a=myFun(1,2,3,4,5);
 // console.log(a);
+
+
 
 // rest operator
 // it must be last formal parameter
@@ -29,19 +32,44 @@
 
 
 // getters and setters
+// try and catch block
 
-let person={
-    fname:'Rahul',
-    lname:'Rajput',
+// let person={
+//     fname:'Rahul',
+//     lname:'Rajput',
 
-    get fullname(){
-        return `${person.fname} ${person.lname}`;
-    },
-    set fullname(value){
-        let parts=value.split(' ');
-        this.fname=parts[0];
-        this.lname=parts[1];
-    }
-}
-person.fullname='Rahul'
-console.log(person.fullname);
+//     get fullname(){
+//         return `${person.fname} ${person.lname}`;
+//     },
+//     set fullname(value){
+//         if(typeof value !== String){
+//             throw new Error("Please Set a String to set a value");
+//         }
+//         let parts=value.split(' ');
+//         this.fname=parts[0];
+//         this.lname=parts[1];
+//     }
+// }
+// try{
+//     person.fullname=1;
+// }
+// catch(e){
+//     alert(e);
+// }
+// console.log(person.fullname);
+
+
+// reducing array
+let arr=[1,2,3,4,5];
+let total=0;
+// for(let value of arr){
+//     total=total+value;
+// }
+// console.log(total);
+
+// accumulator == total
+// currentvalue == value
+// 0 == total=0
+// if we will not add 0 to reduce function then it will take the first value of array
+let totalSum = arr.reduce((accumulator,currentvalue) => accumulator+currentvalue,0);
+console.log(totalSum);
