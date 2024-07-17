4c258515788d12731312f0dcfa67386e82fa61a6
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-17 12:43:46 +0530

    s

---
+++ DOM.js
@@ -0,0 +1,11 @@
+let x=document.getElementById('heading');
+x.innerHTML='Hello RR';
+
+let a=document.getElementsByClassName('paragraph');
+a.innerHTML='Hello';
+
+let b=document.getElementsByTagName('p');
+b.innerHTML='RR';
+
+document.querySelector('header').innerHTML="hii";
+document.querySelector('.paragraph').innerHTML="hiiiiiiiii";
