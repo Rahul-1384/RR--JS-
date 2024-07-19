386873c12b4c9d6655b8755fd40cc21306dd0306
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-19 18:57:19 +0530

    Counter pro completed

--- Counter Project/index.js
+++ Counter Project/index.js
@@ -1,14 +1,20 @@
 let value = document.querySelector('.count');
 let button1 = document.querySelector('.button1');
 let button2 = document.querySelector('.button2');
+let button3 = document.querySelector('.reset');
 
 function minus(){
-    value.textContent = value.textContent-1;
+    value.textContent = parseInt(value.textContent)-1;
 }
 
 function plus(){
-    value.textContent = value.textContent+1;
+    value.textContent = parseInt(value.textContent)+1;
+}
+
+function reset(){
+    value.textContent = 0;
 }
 
 button1.addEventListener('click', minus);
 button2.addEventListener('click', plus);
+button3.addEventListener('click', reset);
