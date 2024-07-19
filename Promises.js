36b1589c4617f5ab776343ac6b8b55462b9f39cd
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-19 12:31:56 +0530

    Promises

---
+++ Promises.js
@@ -0,0 +1,37 @@
+// let myPromise = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log('Hii');
+//     },5000);
+//     // resolve(2004);      // fulfilled
+//     reject(new Error('Hello error is there'));
+// })
+// console.log('First');
+
+
+let myPromise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('Hii');
+    },5000);
+    // resolve(2004);      // fulfilled
+    reject(new Error('Hello error is there'));
+})
+myPromise.then((value) => {console.log(value)});
+myPromise.catch((error) => {console.log('received an error')});
+
+// let myPromise1 = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log('I am 1st Promise');
+//     },2000);
+//     resolve(2004);
+// })
+// let result  = myPromise1.then((value) => {
+//     let myPromise2 = new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             console.log('I am 2nd promise');
+//         },300);
+//         // resolve("Promise2");
+//         reject(new Error('I am error'));
+//     });
+//     return myPromise2;
+// })
+// result.then((value) => console.log(value));
---
+++ callback.js
@@ -0,0 +1,13 @@
+// Callback funtions are those functions that are passed as an argument to the other functions
+
+function myFun(){
+    console.log("hello");
+}
+setTimeout(myFun,3000);
+
+// myFun is callback by setTimeout funciton
+// Note that when ancallback funtion is used as an argument then parenthesis is not allowed in this funciton
+
+
+// But when to use callback funtions
+// with asynchrounous functions we can use the callback funtions
