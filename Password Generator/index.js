d88fb9def726cce204defdb96c25307a00e4fccb
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-23 16:00:21 +0530

    UI Password genenrtor complete

---
+++ Password Generator/index.js
@@ -0,0 +1,9 @@
+let slider = document.querySelector('.slider');
+let copyMsg = document.querySelector('.display-msg');
+let sliderNumber = document.querySelector('.password-number');
+
+let password = "";
+let 
+function sliderFun(){
+    sliderNumber.nodeValue=10;
+}
