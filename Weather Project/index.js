ab0a74f43031d03ef4fe52c99c114e3d373f09cd
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-07-28 12:42:02 +0530

    Access Grant from user

--- Weather Project/index.js
+++ Weather Project/index.js
@@ -1,25 +1,21 @@
-const API_KEY = 'eee37ce1dcfcaac90103d48e243a9e44';
-function renderWeatherInfo(data){
-    let newPara = document.createElement('p');
-    let temperature = (`${data?.main?.temp.toFixed(2)}` - 273.15);
-    newPara.textContent = temperature.toFixed(2);    
-    document.body.appendChild(newPara);
+let grant = document.querySelector('.grant-location-section');
+let button = document.querySelector('[grant-button]');
+
+const API_key = 'eee37ce1dcfcaac90103d48e243a9e44';
+function getGeoLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showUserPosition);
+    }else{
+        console.log('Browser does not support Geolocation');
+    }
 }
 
-// A funtion to fetch data from server.
-async function showWeather(){
-    const lat = 27.4924;
-    const lon = 77.6737;
-    try{
-        
-        let api = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-        let data = await api.json();
-        console.log("Weather data---> " , data);
-        // A function to show the fetched weather data on UI.
-        renderWeatherInfo(data);
-    }
-    catch(err){
-        console.log('Error Found ' + err);
-    }
+let newPara = document.createElement('p');
+grant.appendChild(newPara);
+
+function showUserPosition(position){
+    newPara.textContent = "";
+    newPara.textContent = position.coords.latitude + " " + position.coords.longitude;
 }
-showWeather();
+button.addEventListener('click', getGeoLocation);
+
