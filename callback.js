212278ec23e76bc0d47c9f73578635a1359a25fa
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-10-16 09:57:44 +0530

    Callback

--- callback.js
+++ callback.js
@@ -6,7 +6,7 @@ function myFun(){
 setTimeout(myFun,3000);
 
 // myFun is callback by setTimeout funciton
-// Note that when ancallback funtion is used as an argument then parenthesis is not allowed in this funciton
+// Note that when a callback funtion is used as an argument then parenthesis is not allowed in this funciton
 
 
 // But when to use callback funtions
