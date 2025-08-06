a3a4f06f82c3d5bf0948b0c83a7ea03db9cda4b9
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-08-06 12:26:31 +0530

    feat: Javascripts basics and till Loops

---
+++ PracticeJSMeditab/variables.js
@@ -0,0 +1,21 @@
+// var a = 10;
+// var a = 20;
+// let b = 10;
+// const c = 10;
+
+// function fun(){
+//     let b = 5;
+//     console.log(a);
+//     console.log(b);
+// }
+// fun();
+
+// let x = null;
+// console.log(typeof x)
+
+
+let a = {name: "Rahul", age: 20, sex: "Male", a:{name:"manish", age: 21, sex: "Male"}};
+console.log(a)
+
+let arr = [1,2,3,4,5,"fds"];
+console.log(arr[6] = "fjklsdjf")
