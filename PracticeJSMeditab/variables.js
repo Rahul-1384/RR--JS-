2b2399af44961ece447c8feeea71f6edf94da34f
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-08-06 16:27:45 +0530

    feat: Object cloning(iteration/for_in loop, assign, spread)

--- PracticeJSMeditab/variables.js
+++ PracticeJSMeditab/variables.js
@@ -19,3 +19,38 @@ console.log(a)
 
 let arr = [1,2,3,4,5,"fds"];
 console.log(arr[6] = "fjklsdjf")
+
+
+// Dynamic nature of Objects
+let person = {
+    name: "Rahul",
+    age: 20,
+    sex: "Male",
+}
+console.log(person)
+person.color = "white"
+console.log(person)
+delete person.color
+console.log(person)
+
+// Object Cloning
+// 1) Iteration Or For_in loop
+let person1 = {
+    name: "Rahul",
+    age: 20,
+    sex: "Male",
+}
+let src = {value:1}
+let clone1 = {}
+for(let key in person1){
+    clone1[key] = person1[key]
+}
+console.log("Iteration Cloning",clone1)
+
+// 2) Assign
+let clone2 = Object.assign({}, person1, src)
+console.log("Assign",clone2)
+
+// 3) Spread
+let clone3 = {...person1, ...src}
+console.log("Spread", clone3)
